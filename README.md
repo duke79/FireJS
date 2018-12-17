38f1c94b4cd286454dbc36cd3e8d342e5d61f42a
duke79 <pulkitsingh01@gmail.com>
2018-12-17 23:26:28 +0530

    README.md

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ npm install pyweb
 ## Usage
 
 ```js
-let pw = require('pyweb');
+let pw = require('pyweb')
 
-pw.fire(cmd = "myFunc arg1", function (res) {
-    console.log(res);
+pw.fire(cmd = "myFunc arg1", function (res, err, state) {
+    if (typeof (res) !== 'undefined')
+        console.log(res);
 });
 ```
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ let pw = require('./pyweb')
 
 exports.fire = pw.fire;
 
-pw.fire(cmd = "abc hulo", function (res, err, state) {
-    if (typeof (res) !== 'undefined')
-        console.log(res);
-});
+// pw.fire(cmd = "abc hulo", function (res, err, state) {
+//     if (typeof (res) !== 'undefined')
+//         console.log(res);
+// });
