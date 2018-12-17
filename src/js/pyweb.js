14cc56ca756ef7d2f46fbe5fbe037acd9c8054ca
duke79 <pulkitsingh01@gmail.com>
2018-12-17 22:19:27 +0530

    uuid

--- src/js/pyweb.js
+++ src/js/pyweb.js
@@ -1,28 +1,8 @@
 let window = {}
 window.server = "http://localhost:5555"
 
-var io = require('socket.io-client');
-
-// Either use this (https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript)
-function generateUUID() {
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-    });
-    return uuid;
-}
-
-// Or use this (https://jsfiddle.net/briguy37/2MVFd/)
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-}
+const io = require('socket.io-client');
+const uuidv4 = require('uuid/v4');
 
 //console.log(window.server);
 
@@ -30,14 +10,14 @@ function guid() {
 const socket = io.connect(window.server + '/websocket');
 
 // let socket = io.connect(window.server + '/websocket');
-const req_id = guid();
+const req_id = uuidv4();
 socket.on('connect', function() {
   socket.emit(req_id, "abc gello");
 });
 
 var i = 1; //  set your counter to 1
 
-const req_id_another = generateUUID();
+const req_id_another = uuidv4();
 function myLoop() { //  create a loop function
   setTimeout(function() { //  call a 3s setTimeout when the loop is called    
     i++; //  increment the counter
